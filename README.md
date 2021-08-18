00b88452b76e2176afd0a8af6a848fd8dffbae4e
Pramod Jodhani <mrpramodjodhani@gmail.com>
2021-08-18 13:23:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Description
+
+This is a compatibility plugin for [{Iconic Delivery Slots}](https://iconicwp.com/products/woocommerce-delivery-slots/?utm_source=iconicwp&utm_medium=plugin&utm_campaign=iconic-wds-compat-woochrono) and [WooChrono](https://www.woochrono.com/).
+
+## Changelog
+
+**0.1.0 (2021-08-05)**
+* Initial Release.
---
+++ iconic-woo-compat-woochrono.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Plugin Name:       Iconic WDS compatibility plugin [WooChrono]
+ * Plugin URI:        https://iconicwp.com/products/woocommerce-delivery-slots/?utm_source=iconicwp&utm_medium=plugin&utm_campaign=iconic-wds-compat-woochrono
+ * Description:       Compatibility between {Delivery Slots by Iconic} and WooChrono.
+ * Author:            Iconic
+ * Author URI:        https://iconicwp.com/?utm_source=iconicwp&utm_medium=plugin&utm_campaign=iconic-wds-compat-woochrono
+ * Text Domain:       iconic-compat-72370
+ * Domain Path:       /languages
+ * Version:           0.1.0
+ * GitHub Plugin URI: iconicwp/iconic-wds-compat-woochrono
+ *
+ * Use a random 5 digit number to prevent conflicts. This is used
+ * for function name prefixes (iconic_compat_{54494}_) and the
+ * textdomain. https://numbergenerator.org/random-5-digit-number-generator
+ *
+ * In order to enable automatic updates for the customer, recommend installing
+ * Git Updater: https://github.com/afragen/git-updater
+ *
+ * Make sure to update the git URL for this plugin in the plugin headers.
+ *
+ * Replace anything in curly brackets {}.
+ */
+
+/**
+ * Init.
+ */
+/**
+ * Change the format of shipping method ID.
+ *
+ * @return array
+ */
+function iconic_compat_72370_shipping_replace_shipping_method_id( $shipping_method_options ) {
+	if ( ! function_exists( 'chronofood' ) ) {
+		return $shipping_method_options;
+	}
+
+	foreach ( $shipping_method_options as $key => $method_name ) {
+		if ( 'wc_chrono' === substr( $key, 0, 9 ) ) {
+
+			$new_key = str_replace( 'wc_', '', $key );
+			unset( $shipping_method_options[ $key ] );
+			$shipping_method_options[ $new_key ] = $method_name;
+		}
+	}
+
+	return $shipping_method_options;
+}
+
+add_filter( 'iconic_wds_shipping_method_options', 'iconic_compat_72370_shipping_replace_shipping_method_id', 10 );
